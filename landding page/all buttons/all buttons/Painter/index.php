993f9ec99e31d9e23c2bedea3370d7334067b532
Vaishnavi Kale <84099192+vaishnavikale1518@users.noreply.github.com>
2021-05-28 20:56:31 +0530

    Add files via upload

---
+++ landding page/all buttons/all buttons/Painter/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML>
+<!--
+	Urban by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Servicio</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<div class="logo"><a href="index.html">Yours <span>Servicio</span></a></div>
+				
+			</header>
+
+		<!-- Nav -->
+		
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>Painter</h1>
+						<p>If I had my life to live over again, I'd be a Painter.<br />Click Below To  Get Your Service At Your Location. </p>
+					</header>
+					<a href="http://localhost/p/miniproject/searchpage.php" class="button big scrolly">SEARCH</a>
+				</div>
+			</section>
+
+		<!-- Main -->
+			<div id="main">
+
+			
+
+				<!-- Section -->
+					<section class="wrapper style2">
+						<div class="inner">
+						
+                            <section>
+						<h2>What Painter Does?</h2>
+						<p style="color: black " >
+A Painter prepares walls and other surfaces for painting by using sandpaper, scraping and removing old paint...<br> Painters blend paints and other materials to achieve the right color or texture and paint surfaces according to instructions with various tools..
+                		</p>
+						<ul class="actions">
+							<li><a href="http://localhost/p/miniproject/landding%20page/index.php" class="button alt">Back</a></li>
+						</ul>
+					</section>
+              	<!-- Section              <section>
+						<ul class="checklist">
+							<li>Ipsum nisl sed cursus magna</li>
+							<li>Amet veroeros sed aliquam consequat</li>
+							<li>Tempus lorem adipiscing et lorem</li>
+							<li>Morbi pharetra vitae felis placerat</li>
+							<li>Pharetra nulla risus orci dapibus</li>
+						</ul>
+					</section>--> 
+                            
+						</div>
+					</section>
+                
+                <!-- Add icon library -->
+
+
+
+
+                
+       
+                
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
