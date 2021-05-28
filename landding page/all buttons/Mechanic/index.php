de2ce2268a1c2b5a44c77b9c1e72ceacf0f85547
Vaishnavi Kale <84099192+vaishnavikale1518@users.noreply.github.com>
2021-05-28 20:53:22 +0530

    Add files via upload

---
+++ landding page/all buttons/Mechanic/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE HTML>
+<!--
+	Urban by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Servicio</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<div class="logo"><a href="index.html">Yours <span>Servicio</span></a></div>
+				
+			</header>
+
+		<!-- Nav -->
+		
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>Mechanic</h1>
+						<p>If I had my life to live over again, I'd be a Mechanic.<br />Click Below To  Get Your Service At Your Location. </p>
+					</header>
+					<a href="http://localhost/p/miniproject/searchpage.php" class="button big scrolly">SEARCH</a>
+				</div>
+			</section>
+
+		<!-- Main -->
+			<div id="main">
+
+			
+
+				<!-- Section -->
+					<section class="wrapper style2">
+						<div class="inner">
+						
+                            <section>
+						<h2>What Mechanic Does?</h2>
+						<p style="color: black " >A person who Repairs and Maintains Machinery, motors, etc.<br>An "Automobile Mechanic". A worker who is skilled in the use of tools, machines, equipment, etc... 
+                		</p>
+						<ul class="actions">
+							<li><a href="http://localhost/p/miniproject/landding%20page/index.php" class="button alt">Back</a></li>
+						</ul>
+					</section>
+              	<!-- Section              <section>
+						<ul class="checklist">
+							<li>Ipsum nisl sed cursus magna</li>
+							<li>Amet veroeros sed aliquam consequat</li>
+							<li>Tempus lorem adipiscing et lorem</li>
+							<li>Morbi pharetra vitae felis placerat</li>
+							<li>Pharetra nulla risus orci dapibus</li>
+						</ul>
+					</section>--> 
+                            
+						</div>
+					</section>
+                
+                <!-- Add icon library -->
+
+
+
+
+                
+       
+                
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ landding page/all buttons/Packaging/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE HTML>
+<!--
+	Urban by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Servicio</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <script src="jquery-2.1.4.js"></script>
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<div class="logo"><a href="index.html">Yours <span>Servicio</span></a></div>
+				
+			</header>
+
+		<!-- Nav -->
+		
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>Packaging</h1>
+						<p>If I had my life to live over again, I'd be in a Packaging service.<br />Click Below To  Get Your Service At Your Location. </p>
+					</header>
+					<a href="http://localhost/p/miniproject/searchpage.php" class="button big scrolly">SEARCH</a>
+				</div>
+			</section>
+
+		<!-- Main -->
+			<div id="main">
+
+			
+
+				<!-- Section -->
+					<section class="wrapper style2">
+						<div class="inner">
+						
+                            <section>
+						<h2>What Packaging Service Does?</h2>
+						<p style="color: black " >Packaging also refers to the process of designing, evaluating, and producing packages.<br> Packaging can be described as a coordinated system of preparing goods for transport, warehousing, logistics, sale, and end use. Packaging contains, protects, preserves, transports, informs, and sells.
+                		</p>
+						<ul class="actions">
+							<li><a href="http://localhost/p/miniproject/landding%20page/index.php" class="button alt">Back</a></li>
+						</ul>
+					</section>
+              	<!-- Section              <section>
+						<ul class="checklist">
+							<li>Ipsum nisl sed cursus magna</li>
+							<li>Amet veroeros sed aliquam consequat</li>
+							<li>Tempus lorem adipiscing et lorem</li>
+							<li>Morbi pharetra vitae felis placerat</li>
+							<li>Pharetra nulla risus orci dapibus</li>
+						</ul>
+					</section>--> 
+                            
+						</div>
+					</section>
+                
+                <!-- Add icon library -->
+
+
+
+
+                
+       
+                
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
